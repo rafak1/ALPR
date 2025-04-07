be5b4184db9662ae22132477261c0a92577b24f8
Rafau <rafalkajca11@gmail.com>
2025-04-07 02:22:25 +0200

    crnn

---
+++ OCR/CRNN/crnn.py
@@ -0,0 +1,54 @@
+import torch.nn as nn
+from . import utils
+
+class CRNN(nn.Module):
+    def __init__(self, nh):
+        super(CRNN, self).__init__()
+
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 1, 1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.ReLU(True),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.ReLU(True),
+            nn.MaxPool2d((2, 1), (2, 1)),
+
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.MaxPool2d((4, 1), (4, 1))
+        )
+
+        self.rnn = nn.Sequential(
+            nn.LSTM(128, nh * 2, bidirectional=False),
+            #GetLSTMOutput(),
+            #nn.LSTM(nh * 2, nh, bidirectional=False)
+        )
+
+        nclass = len(utils.alphabet) + 1
+        self.embedding = nn.Linear(nh * 2, nclass)
+
+    def forward(self, x):
+        #print(f"before {x.size()}")
+        x = self.cnn(x)
+        #print(f"after {x.size()}")
+        x = x.squeeze(2).permute(2, 0, 1)  # [W, B, C]
+        #print(f"after squeeze {x.size()} {x.dim()}")
+        x, _ = self.rnn(x)
+        x = self.embedding(x)
+        #print(f"after embedding {x.size()} {x.dim()}")
+        return x  # [W, B, nclass]
+    
+
+class GetLSTMOutput(nn.Module):
+    def forward(self, x):
+        out, _ = x
+        return out
---
+++ OCR/run_crnn.py
@@ -0,0 +1,21 @@
+import os
+import torch
+import CRNN.crnn as crnn
+import CRNN.train as train
+from torch.utils.data import DataLoader
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    path = current_dir + '/CRNN/dataset_final'
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = crnn.CRNN(nh=256).to(device)
+    train.train(path, model, device, epochs=150, batch_size=5)
+
+    test_dataset = train.PlateDataset(path + '/test')
+    test_loader = DataLoader(test_dataset, batch_size=5, shuffle=False, collate_fn=train.collate_fn)
+
+    # save the model
+    torch.save(model.state_dict(), 'crnn_model.pth')
+    
+    train.test(model, test_loader, device)
